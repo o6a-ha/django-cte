79eb0259e3b402fd859493936e7affd92b861dcc
globaltrouble <www.globaltrouble@gmail.com>
2019-07-15 18:30:37 +0300

    added from_query method to CTEQuery

--- django_cte/query.py
+++ django_cte/query.py
@@ -18,6 +18,60 @@ class CTEQuery(Query):
         super(CTEQuery, self).__init__(*args, **kwargs)
         self._with_ctes = []
 
+    @classmethod
+    def from_query(cls, other):
+        new = cls(
+            model=other.model,
+            where=other.where_class
+        )
+
+        new.model = other.model
+        new.alias_refcount = other.alias_refcount
+        new.alias_map = other.alias_map
+        new.external_aliases = other.external_aliases
+        new.table_map = other.table_map
+        new.default_cols = other.default_cols
+        new.default_ordering = other.default_ordering
+        new.standard_ordering = other.standard_ordering
+        new.used_aliases = other.used_aliases
+        new.filter_is_sticky = other.filter_is_sticky
+
+        new.select = other.select
+        new.tables = other.tables
+        new.where = other.where
+
+        new.where_class = other.where_class
+        new.group_by = other.group_by
+        new.order_by = other.order_by
+        new.low_mark = other.low_mark
+        new.high_mark = other.high_mark
+        new.distinct = other.distinct
+        new.distinct_fields = other.distinct_fields
+        new.select_for_update = other.select_for_update
+        new.select_for_update_nowait = other.select_for_update_nowait
+
+        new.select_related = other.select_related
+        new.max_depth = other.max_depth
+
+        new.values_select = other.values_select
+
+        new._annotations = other._annotations
+        new.annotation_select_mask = other.annotation_select_mask
+        new._annotation_select_cache = other._annotation_select_cache
+
+        new._extra = other._extra
+        new.extra_select_mask = other.extra_select_mask
+        new._extra_select_cache = other._extra_select_cache
+
+        new.extra_tables = other.extra_tables
+        new.extra_order_by = other.extra_order_by
+
+        new.deferred_loading = other.deferred_loading
+
+        new.context = other.context
+
+        return new
+
     def combine(self, other, connector):
         if other._with_ctes:
             if self._with_ctes:
@@ -63,7 +117,7 @@ class CTECompiler(object):
 
     @classmethod
     def generate_sql(cls, connection, query, as_sql):
-        if query.combinator:
+        if getattr(query, 'combinator', False):
             return as_sql()
 
         ctes = []
